213eaee53b326d59617528619ce682cfea93d7a1
EGtraveller <34904182+EGtraveller@users.noreply.github.com>
2018-11-16 11:24:15 +0800

    ByteArrayOutputStream.java
    ByteArrayOutputStream.java

--- ByteArrayOutputStream.java
+++ ByteArrayOutputStream.java
@@ -1,9 +1,9 @@
 package com.hxg.TestIO;
 /**
- * ´´½¨Ô´
- * Ñ¡ÔñÁ÷
- * ²Ù×÷
- * ÊÍ·Å×ÊÔ´£¨¿ÉÒÔ²»Òª£¬·ÅÔÚÕâÀïÊÇÒòÎª´úÂë·ç¸ñÍ³Ò»£©
+ * åˆ›å»ºæº
+ * é€‰æ‹©æµ
+ * æ“ä½œ
+ * é‡Šæ”¾èµ„æºï¼ˆå¯ä»¥ä¸è¦ï¼Œæ”¾åœ¨è¿™é‡Œæ˜¯å› ä¸ºä»£ç é£Žæ ¼ç»Ÿä¸€ï¼‰
  */
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -18,18 +18,18 @@ import java.io.Reader;
 
 public class IOTest10 {
 	public static void main(String[] args) {
-		//½¨Á¢ÁªÏµ
+		//å»ºç«‹è”ç³»
 		byte[] dest = null;
-		//Ñ¡ÔñÁ÷
+		//é€‰æ‹©æµ
 		ByteArrayOutputStream bos = null;
 		try {
 			bos = new ByteArrayOutputStream();
-			//¶ÁÈ¡ÎÄ¼þ
+			//è¯»å–æ–‡ä»¶
 			String msg = "talk is cheap show me the code";
 			byte[] datas = msg.getBytes();
 			bos.write(datas,0,datas.length);
 			bos.flush();
-			//»ñÈ¡Êý¾Ý
+			//èŽ·å–æ•°æ®
 			dest = bos.toByteArray();
 			System.out.println(dest.length +"--->"+ new String(dest,0,bos.size()));
 		} catch (IOException e) {
